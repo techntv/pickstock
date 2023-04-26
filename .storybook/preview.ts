428bbb0380da180748ee1114fc8bae166df69ad9
Vinh Nguyen <techntvz@gmail.com>
2023-04-26 18:50:25 +0700

    add charkaui and storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,14 @@
+import { theme } from "../src/config/theme";
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  controls: { expanded: true },
+  chakra: {
+    theme,
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import ' app/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { AppProvider } from "@/providers/app";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
+  );
+};
+
+export default App;
